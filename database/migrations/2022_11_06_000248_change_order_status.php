171dd5316d826af9184841e7d81779ed3d7e0ac0
bachtung144 <bachtung144007@gmail.com>
2022-11-06 14:44:59 +0700

    Add logic for checkout

---
+++ database/migrations/2022_11_06_000248_change_order_status.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+
+class ChangeOrderStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            DB::statement("ALTER TABLE orders CHANGE COLUMN status status ENUM('1', '2', '3') DEFAULT '1'");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            DB::statement("ALTER TABLE orders CHANGE COLUMN status status ENUM('ordered', 'delivered', 'canceled') DEFAULT 'ordered'");
+        });
+    }
+}
