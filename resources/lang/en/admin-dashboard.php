a7d0724ad0988dd440224e0f7b361e01f46118f7
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2022-11-15 08:18:51 +0700

    feat: add admin dashboard

---
+++ resources/lang/en/admin-dashboard.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'total_revenue' => 'Total Revenue',
+    'update' => 'Updated Now',
+    'total_sales' => 'Total Sales',
+    'today_revenue' => 'Today Revenue',
+    'today_sales' => 'Today Sales',
+];
